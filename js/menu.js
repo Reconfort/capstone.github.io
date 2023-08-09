2fcc080a4a16e7929a5c4ef090c554ff59826718
Reconfort Daniel <reconfortdanny@gmail.com>
2023-08-09 18:07:22 +0200

    Add contribution, preview link on README and Menu Link

--- js/menu.js
+++ js/menu.js
@@ -13,33 +13,33 @@ openMenu.addEventListener('click', () => {
 });
 
 // get unique link id
-const link1 = document.querySelector('.link1');
-const link2 = document.querySelector('.link2');
-const link3 = document.querySelector('.link3');
-const link4 = document.querySelector('.link4');
-const link5 = document.querySelector('.link5');
+const links1 = document.querySelector('.link1');
+const links2 = document.querySelector('.link2');
+const links3 = document.querySelector('.link3');
+const links4 = document.querySelector('.link4');
+const links5 = document.querySelector('.link5');
 
-link1.addEventListener('click', () => {
+links1.addEventListener('click', () => {
   menuContainer.style.transition = '0.5s';
   menuContainer.style.transform = 'translateY(-200%)';
 });
 
-link2.addEventListener('click', () => {
+links2.addEventListener('click', () => {
   menuContainer.style.transition = '0.5s';
   menuContainer.style.transform = 'translateY(-200%)';
 });
 
-link3.addEventListener('click', () => {
+links3.addEventListener('click', () => {
   menuContainer.style.transition = '0.5s';
   menuContainer.style.transform = 'translateY(-200%)';
 });
 
-link4.addEventListener('click', () => {
+links4.addEventListener('click', () => {
   menuContainer.style.transition = '0.5s';
   menuContainer.style.transform = 'translateY(-200%)';
 });
 
-link5.addEventListener('click', () => {
+links5.addEventListener('click', () => {
   menuContainer.style.transition = '0.5s';
   menuContainer.style.transform = 'translateY(-200%)';
 });
