b7ff9387f6a80849303a5b6712f9bd0b44712de0
Reconfort Daniel <reconfortdanny@gmail.com>
2023-08-09 18:05:21 +0200

    Add contribution, preview link on README and Menu Link

--- js/menu.js
+++ js/menu.js
@@ -1,46 +1,45 @@
-const menuContainer = document.querySelector(".menuContainer");
-const closeMenu = document.querySelector(".closeMenu");
-const openMenu = document.querySelector(".menu-bar");
+const menuContainer = document.querySelector('.menuContainer');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.menu-bar');
 
-closeMenu.addEventListener("click", () => {
-  menuContainer.style.transition = "1s";
-  menuContainer.style.transform = "translateY(-200%)";
+closeMenu.addEventListener('click', () => {
+  menuContainer.style.transition = '1s';
+  menuContainer.style.transform = 'translateY(-200%)';
 });
 
-openMenu.addEventListener("click", () => {
-  menuContainer.style.transition = "0.5s";
-  menuContainer.style.transform = "translateY(0)";
+openMenu.addEventListener('click', () => {
+  menuContainer.style.transition = '0.5s';
+  menuContainer.style.transform = 'translateY(0)';
 });
 
 // get unique link id
-const link1 = document.querySelector(".link1");
-const link2 = document.querySelector(".link2");
-const link3 = document.querySelector(".link3");
-const link4 = document.querySelector(".link4");
-const link5 = document.querySelector(".link5");
+const link1 = document.querySelector('.link1');
+const link2 = document.querySelector('.link2');
+const link3 = document.querySelector('.link3');
+const link4 = document.querySelector('.link4');
+const link5 = document.querySelector('.link5');
 
-
-link1.addEventListener("click", () => {
-  menuContainer.style.transition = "0.5s";
-  menuContainer.style.transform = "translateY(-200%)";
+link1.addEventListener('click', () => {
+  menuContainer.style.transition = '0.5s';
+  menuContainer.style.transform = 'translateY(-200%)';
 });
 
-link2.addEventListener("click", () => {
-  menuContainer.style.transition = "0.5s";
-  menuContainer.style.transform = "translateY(-200%)";
+link2.addEventListener('click', () => {
+  menuContainer.style.transition = '0.5s';
+  menuContainer.style.transform = 'translateY(-200%)';
 });
 
-link3.addEventListener("click", () => {
-  menuContainer.style.transition = "0.5s";
-  menuContainer.style.transform = "translateY(-200%)";
+link3.addEventListener('click', () => {
+  menuContainer.style.transition = '0.5s';
+  menuContainer.style.transform = 'translateY(-200%)';
 });
 
-link4.addEventListener("click", () => {
-  menuContainer.style.transition = "0.5s";
-  menuContainer.style.transform = "translateY(-200%)";
+link4.addEventListener('click', () => {
+  menuContainer.style.transition = '0.5s';
+  menuContainer.style.transform = 'translateY(-200%)';
 });
 
-link5.addEventListener("click", () => {
-  menuContainer.style.transition = "0.5s";
-  menuContainer.style.transform = "translateY(-200%)";
+link5.addEventListener('click', () => {
+  menuContainer.style.transition = '0.5s';
+  menuContainer.style.transform = 'translateY(-200%)';
 });
